0f1285dbd77da1902ac85dc5400e310ba275d395
Lemmore <Leuchteintraum@gmail.com>
2019-04-23 17:37:04 -0700

    Update bash_step1.sh

--- bash_step1.sh
+++ bash_step1.sh
@@ -8,10 +8,16 @@
 
 SEED=$(head -n ${SGE_TASK_ID} samples.txt | tail -n 1)
 GZ_DIR="chrX_data/samples/"
+GTF="chrX_data/genes/chrX.gtf"
 GZ_1="_chrX_1.fastq.gz"
 GZ_2="_chrX_2.fastq.gz"
 
+module load hisat2
+module load samtools
+module load stringtie
 
-###hisat2 -p 1 --dta -x chrX_data/indexes/chrX_tran -1 ${GZ_DIR}${SAMPLE}${GZ_1} -2 ${GZ_DIR}${SAMPLE}${GZ_2} -S ${SAMPL$
+##hisat2 -p 1 --dta -x chrX_data/indexes/chrX_tran -1 ${GZ_DIR}${SEED}${GZ_1} -2 ${GZ_DIR}${SEED}${GZ_2} -S ${SEED}_chrX.sam
 
-echo ${GZ_DIR}${SEED}${GZ_1}
+##samtools sort -@ 1 -o ${SEED}_chrX.bam ${SEED}_chrX.sam
+
+stringtie -p 1 -G ${GTF} -o ${SEED}_chrX.gtf -l ${SEED} ${SEED}_chrX.bam
