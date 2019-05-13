4913eb1e8ee19f11f812d5c5456d699bcab1f15c
aekimura <43656866+aekimura@users.noreply.github.com>
2019-05-13 16:49:43 -0700

    Update bash_step3.sh

--- bash_step3.sh
+++ bash_step3.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-#$ -N Step_1
-#$ -t 1-12
+
+#$ -N Steps4-5
+#$ -t 1-8
 #$ -ckpt restart
 #$ -q bio,abio*,pub64,free64,epyc
-#$ -pe openmp 1
-#$ -R y
+#$ -pe openmp
 
-SEED=$(head -n ${SGE_TASK_ID} samples.txt | tail -n 1)
-BGN_DATA_DIR="ballgown/"
+SAMPLE=$(head -n ${SGE_TASK_ID} samples.txt | tail -n 1)
 
 module load stringtie
-
-stringtie -e -B -p 1 -G stringtie_merged.gtf -o ${BGN_DATA_DIR}${SEED}/${SEED}_chrX.gtf ${SEED}_chrX.bam
+stringtie -e -B -p 1 -G stringtie_merged.gtf -o ballgown/${SAMPLE}/${SAMPLE}_S288C.gtf ${SAMPLE}_S288C.bam
