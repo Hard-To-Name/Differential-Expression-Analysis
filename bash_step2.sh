bd84e7798924c68fbc852e11b3d44e6a289de7ae
aekimura <43656866+aekimura@users.noreply.github.com>
2019-05-13 16:45:54 -0700

    Update bash_step2.sh

--- bash_step2.sh
+++ bash_step2.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-#$ -N Step_2
-#$ -ckp restart
+#$ -N Steps4-5
+#$ -t 1-8
+#$ -ckpt restart
 #$ -q bio,abio*,pub64,free64,epyc
 #$ -pe openmp 1
 
-GZ_DIR="chrX_data/samples/"
-GTF="chrX_data/genes/chrX.gtf"
-GFF_COMPARE="/data/users/duanr1/bin/gffcompare/gffcompare"
 
 module load stringtie
+stringtie --merge -p 1 -G S288C_data/genes/S288C.gtf -o stringtie_merged.gtf  S288C_data/mergelist.txt
 
-stringtie --merge -p 1 -G ${GTF} -o stringtie_merged.gtf chrX_data/mergelist.txt
-
-${GFF_COMPARE} -r ${GTF} -G -o merged stringtie_merged.gtf
+modlue load gffcompare
+gffcompare -r S288C_data/genes/S288C.gtf -G -o merged stringtie_merged.gtf
