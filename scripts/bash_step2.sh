1438293c8b6ea57131c26d055fb7a6be2b26f6da
Lemmore <Leuchteintraum@gmail.com>
2019-06-08 16:31:18 -0700

    Update bash_step2.sh

--- scripts/bash_step2.sh
+++ scripts/bash_step2.sh
@@ -14,6 +14,8 @@ conda activate hisat2
 
 stringtie --merge -p 1 -G ${REFERENCE}S288C.gtf -o ${REFERENCE}stringtie_merged.gtf ${REFERENCE}mergelist.txt
 
+gffread -E ${REFERENCE}stringtie_merged.gtf -o- > ${REFERENCE}stringtie_merged.gff3 # for bedtools use
+
 /data/users/duanr1/bin/gffcompare/gffcompare -r ${REFERENCE}S288C.gtf -G -o ${REFERENCE}merged ${REFERENCE}stringtie_merged.gtf
 
 conda deactive
