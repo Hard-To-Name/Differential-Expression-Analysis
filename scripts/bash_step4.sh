77e18a22b7fe987e0a12754c65c8dba6e4a89f2c
Lemmore <Leuchteintraum@gmail.com>
2019-06-10 15:07:57 -0700

    Update bash_step4.sh

--- scripts/bash_step4.sh
+++ scripts/bash_step4.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #$ -N Bedtools
+#$ -t 1-10
 #$ -ckpt restart
 #$ -q bio,abio*,pub64,free64,epyc
 #$ -pe openmp 1
@@ -9,7 +10,18 @@ source ~/.miniconda3testrc
 conda activate bedtools
 
 REFERENCE=../S288C_reference_genome_R64-2-1_20150113/
+SIGNIFICANT=$(head -n ${SGE_TASK_ID} ${REFERENCE}significant_transcripts.txt | tail -n 1)
 
-bedtools getfasta -fi ${REFERENCE}S288C_reference_sequence_R64-2-1_20150113_new.fsa -bed ${REFERENCE}stringtie_merged.gtf -fo ${REFERENCE}bedtools_output.fasta
+echo ${SIGNIFICANT}.gtf
+
+if [ ! -d ${REFERENCE}significant ]; then
+  mkdir -p ${REFERENCE}significant;
+fi
+
+grep -a ${SIGNIFICANT} ${REFERENCE}stringtie_merged.gtf > ${REFERENCE}significant/${SIGNIFICANT}.gtf
+
+bedtools getfasta -fi ${REFERENCE}S288C_reference_sequence_R64-2-1_20150113_new.fsa -bed ${REFERENCE}significant/${SIGNIFICANT}.gtf -fo ${REFERENCE}significant/${SIGNIFICANT}.fasta
+
+sed -i '/>/d' ${REFERENCE}significant/${SIGNIFICANT}.fasta
 
 conda deactivate
