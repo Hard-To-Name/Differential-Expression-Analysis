3120f84b7f4e5c22bbf8611e5954137b31ef44c0
Lemmore <Leuchteintraum@gmail.com>
2019-06-11 16:43:01 -0700

    Update bash_step4-1.sh

--- scripts/bash_step4-1.sh
+++ scripts/bash_step4-1.sh
@@ -12,16 +12,14 @@ conda activate bedtools
 REFERENCE=../S288C_reference_genome_R64-2-1_20150113/
 SIGNIFICANT=$(head -n ${SGE_TASK_ID} ${REFERENCE}significant1.txt | tail -n 1)
 
-echo ${SIGNIFICANT}.gtf
-
-if [ ! -d ${REFERENCE}significant ]; then
-  mkdir -p ${REFERENCE}significant;
+if [ ! -d ${REFERENCE}significant1 ]; then
+  mkdir -p ${REFERENCE}significant1;
 fi
 
-grep -a ${SIGNIFICANT} ${REFERENCE}stringtie_merged.gtf > ${REFERENCE}significant/${SIGNIFICANT}.gtf
+grep -a ${SIGNIFICANT} ${REFERENCE}stringtie_merged.gtf > ${REFERENCE}significant1/${SIGNIFICANT}.gtf
 
-bedtools getfasta -fi ${REFERENCE}S288C_reference_sequence_R64-2-1_20150113_new.fsa -bed ${REFERENCE}significant/${SIGNIFICANT}.gtf -fo ${REFERENCE}significant/${SIGNIFICANT}.fasta
+bedtools getfasta -fi ${REFERENCE}S288C_reference_sequence_R64-2-1_20150113_new.fsa -bed ${REFERENCE}significant1/${SIGNIFICANT}.gtf -fo ${REFERENCE}significant/${SIGNIFICANT}.fasta
 
-sed -i '/>/d' ${REFERENCE}significant/${SIGNIFICANT}.fasta
+sed -i '/>/d' ${REFERENCE}significant1/${SIGNIFICANT}.fasta
 
 conda deactivate
